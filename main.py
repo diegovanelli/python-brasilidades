82beaa33d19f04bb863f093dbc8567ba5a5d5818
Diego <diegodvanelli@gmail.com>
2021-09-19 07:16:10 -0300

    Brasilidades

---
+++ main.py
@@ -0,0 +1,13 @@
+from cpf_cnpj import Documento
+from validate_docbr import CNPJ
+
+#cpf_um = Cpf("15316264754")
+#print(cpf_um)
+
+exemplo_cnpj = "35379838000112"
+#exemplo_cpf = "11111111112"
+
+#cnpj = CNPJ()
+#print(cnpj.validate(exemplo_cnpj))
+documento = Documento.cria_documento(exemplo_cnpj)
+print(documento)
